f44b05920a3658868b7a1f35c684eb3b9cdf2341
victorcsm1995 <victorcorrea.moreira@gmail.com>
2023-03-22 10:47:52 -0300

    Atualizando com arquivo local

--- add.js
+++ add.js
@@ -1,3 +1,4 @@
-console.log("Rodando arquivo de games")
-console.log("tudo ta rodando")
-console.log("Mudança do Homem")
+console.log("Rodando arquivo de games");
+console.log("tudo ta rodando");
+console.log("Mudança do Homem");
+console.log("Atualizando com codigo local");
