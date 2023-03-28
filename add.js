536927877b433b6172e2f55e90e27d2d2c0e65ab
victorcsm1995 <victorcorrea.moreira@gmail.com>
2023-03-28 10:13:52 -0300

    script no index e mexendo no add.js

--- add.js
+++ add.js
@@ -4,3 +4,10 @@ console.log("Mudança do Homem");
 console.log("Atualizando com codigo local");
 console.log("Teste do git pelo cmd");
 console.log("Fazendo commit pelo cmd");
+
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
